e6aaf44651b9b1685783573738cf29923dd1b623
Shunsuke Sato <shunsuke.sato26@gmail.com>
2016-11-16 16:59:29 +0100

    Fixed a bug in the dipole moment.

--- src/dipole.f90
+++ src/dipole.f90
@@ -4,21 +4,24 @@
 ! https://opensource.org/licenses/mit-license.php   !
 !---------------------------------------------------!
 !-------10--------20--------30--------40--------50--------60--------70--------80--------90
-subroutine dipole(dipole ,norm)
+subroutine dipole(dipole_t ,norm_t)
   use global_variables
   implicit none
-  real(dp),intent(out) :: dipole,norm
+  real(dp),intent(out) :: dipole_t ,norm_t
   integer :: ix,iy
   real(dp) :: tmp
 
 
-  dipole = 0d0; norm = 0d0
+  dipole_t = 0d0; norm_t = 0d0
   do iy = 0,NR
     do ix = 0,Nx
-      dipole = dipole + xn(ix)*abs(zwfn(ix,iy))**2
-      norm = norm + xn(ix)*abs(zwfn(ix,iy))**2
+      dipole_t = dipole_t + xn(ix)*abs(zwfn(ix,iy))**2
+      norm_t = norm_t + abs(zwfn(ix,iy))**2
     end do
   end do
+
+  dipole_t = dipole_t*dx*dr
+  norm_t = norm_t*dx*dr
 
 
   return
