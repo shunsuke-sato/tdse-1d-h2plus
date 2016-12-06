39a8baedcf8ddaf72a3e6d7b0843c6ace6535b7d
Shunsuke Sato <shunsuke.sato26@gmail.com>
2016-12-06 14:58:22 +0100

    Changed initial condition to delta-kick.

--- src/preparation_RT.f90
+++ src/preparation_RT.f90
@@ -7,11 +7,20 @@
 subroutine preparation_RT
   use global_variables
   implicit none
+  integer :: ix,iy
+
 
   allocate(zwfn(0:Nx,0:NR))
   allocate(dipole_t(0:Nt_iter),norm_t(0:Nt_iter))
 !Initial condition
-  zwfn = wfn ! GS wavefunction
+!  zwfn = wfn ! GS wavefunction
+
+!momentum kick
+  do iy=0,NR
+    do ix=0,Nx
+      zwfn(ix,iy) = exp(zI*kick_mom*xn(ix))*wfn(ix,iy) 
+    end do
+  end do
 
   return
 end subroutine preparation_RT
